39742df409c73a120c7b034121d3c00066af8a23
Theo de Raadt <deraadt@openbsd.org>
1996-05-30 09:53:35 +0000

    $OpenBSD$ tag and few spelling errors

--- usr.sbin/ypserv/yppush/yppush_proc.c
+++ usr.sbin/ypserv/yppush/yppush_proc.c
@@ -1,3 +1,5 @@
+/*	$OpenBSD: yppush_proc.c,v 1.2 1996/05/30 09:53:21 deraadt Exp $ */
+
 /*
  * Copyright (c) 1996 Mats O Jansson <moj@stacken.kth.se>
  * All rights reserved.
@@ -30,7 +32,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: yppush_proc.c,v 1.0 1996/01/15 01:15:10 moj Exp $";
+static char rcsid[] = "$OpenBSD: yppush_proc.c,v 1.2 1996/05/30 09:53:21 deraadt Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
