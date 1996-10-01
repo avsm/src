1c43a3be76158d182a57a95408d72d86b1622486
branch-fixup <branch-fixup>
1996-10-01 02:05:57 +0000

    Add files from parent branch HEAD: 	bin/ksh/tests/regress.t 	bin/ksh/tests/unclass1.t 	bin/ksh/tests/version.t 	bin/pdksh/BUG-REPORTS 	bin/pdksh/CONTRIBUTORS 	bin/pdksh/ChangeLog 	bin/pdksh/IAFA-PACKAGE 	bin/pdksh/NEWS 	bin/pdksh/NOTES 	bin/pdksh/PROJECTS 	bin/pdksh/README 	bin/pdksh/c_ksh.c 	bin/pdksh/edit.c 	bin/pdksh/exec.c 	bin/pdksh/ksh_stat.h 	bin/pdksh/ksh_time.h 	bin/pdksh/ksh_times.h 	bin/pdksh/lex.c 	bin/pdksh/lex.h 	bin/pdksh/main.c 	bin/pdksh/sh.h 	bin/pdksh/siglist.sh 	bin/pdksh/syn.c 	bin/pdksh/tests/regress.t 	bin/pdksh/tests/unclass1.t 	bin/pdksh/tests/version.t 	bin/pdksh/tty.c 	bin/pdksh/var.c 	bin/pdksh/version.c

---
+++ bin/pdksh/version.c
@@ -0,0 +1,10 @@
+/*	$OpenBSD: version.c,v 1.3 1996/10/01 02:05:54 downsj Exp $	*/
+
+/*
+ * value of $KSH_VERSION (or $SH_VERSION)
+ */
+
+#include "sh.h"
+
+const char ksh_version [] =
+	"@(#)PD KSH v5.2.9 96/09/30";
