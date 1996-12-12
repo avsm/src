6600c7a4f85294181d8f6053a66e3d6b3e36749f
bitblt <bitblt>
1996-12-12 16:23:05 +0000

    *** empty log message ***

--- usr.sbin/tcpdump/ntp.h
+++ usr.sbin/tcpdump/ntp.h
@@ -1,6 +1,4 @@
-/*	$OpenBSD: ntp.h,v 1.4 1996/07/13 11:01:13 mickey Exp $	*/
-
-/* Header: ntp.h,v 1.3 95/05/04 17:52:49 mccanne Exp */
+/* $Header: ntp.h,v 1.3 95/05/04 17:52:49 mccanne Exp $ */
 
 /*
  * Based on ntp.h from the U of MD implementation
