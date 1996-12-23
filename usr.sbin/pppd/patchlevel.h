d138b86eb17b1c6c7133261c97889ff92e5244bd
branch-fixup <branch-fixup>
1996-12-23 13:22:49 +0000

    Add files from parent branch HEAD: 	usr.sbin/pppd/cbcp.c 	usr.sbin/pppd/cbcp.h 	usr.sbin/pppd/ccp.c 	usr.sbin/pppd/chap.h 	usr.sbin/pppd/demand.c 	usr.sbin/pppd/ipcp.c 	usr.sbin/pppd/ipxcp.c 	usr.sbin/pppd/lcp.c 	usr.sbin/pppd/lcp.h 	usr.sbin/pppd/patchlevel.h 	usr.sbin/pppd/pathnames.h 	usr.sbin/pppd/pppd.8 	usr.sbin/pppd/pppd.h 	usr.sbin/pppd/upap.c

---
+++ usr.sbin/pppd/patchlevel.h
@@ -0,0 +1,7 @@
+/*	$OpenBSD: patchlevel.h,v 1.4 1996/12/23 13:22:46 mickey Exp $	*/
+
+#define	PATCHLEVEL	0
+
+#define VERSION		"2.3"
+#define IMPLEMENTATION        "beta3"
+#define DATE          "8 Oct 96"
