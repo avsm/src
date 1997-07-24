bdd1780d2a907e04981d2931a81f9e1493cf809b
branch-fixup <branch-fixup>
1997-07-24 14:48:57 +0000

    Add files from parent branch HEAD: 	sys/arch/alpha/include/limits.h 	sys/arch/amiga/include/limits.h 	sys/arch/arc/include/limits.h 	sys/arch/atari/include/limits.h 	sys/arch/hp300/include/limits.h 	sys/arch/i386/include/limits.h 	sys/arch/mac68k/include/limits.h 	sys/arch/mvme68k/include/limits.h 	sys/arch/mvme88k/include/limits.h 	sys/arch/pc532/include/limits.h 	sys/arch/pmax/include/limits.h 	sys/arch/vax/include/limits.h 	sys/arch/wgrisc/include/limits.h

---
+++ sys/arch/mac68k/include/limits.h
@@ -0,0 +1,9 @@
+/*	$OpenBSD: limits.h,v 1.3 1997/07/24 14:48:49 deraadt Exp $	*/
+/*	$NetBSD: limits.h,v 1.6 1994/10/26 08:46:34 cgd Exp $	*/
+
+#ifndef _MACHINE_LIMITS_H_
+#define _MACHINE_LIMITS_H_
+
+#include <m68k/limits.h>
+
+#endif /* _MACHINE_LIMITS_H_ */
