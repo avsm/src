f9afb878a9c6dfcc0f55c81d4906c0f906ff26dd
branch-fixup <branch-fixup>
1998-01-17 20:30:33 +0000

    Add files from parent branch HEAD: 	usr.sbin/pppd/Makefile 	usr.sbin/pppd/auth.c 	usr.sbin/pppd/ccp.c 	usr.sbin/pppd/chap.c 	usr.sbin/pppd/chap_ms.c 	usr.sbin/pppd/chap_ms.h 	usr.sbin/pppd/chat/chat.8 	usr.sbin/pppd/chat/chat.c 	usr.sbin/pppd/demand.c 	usr.sbin/pppd/fsm.c 	usr.sbin/pppd/ipxcp.c 	usr.sbin/pppd/lcp.c 	usr.sbin/pppd/main.c 	usr.sbin/pppd/options.c 	usr.sbin/pppd/patchlevel.h 	usr.sbin/pppd/pppd.8 	usr.sbin/pppd/pppd.h 	usr.sbin/pppd/upap.c

---
+++ usr.sbin/pppd/patchlevel.h
@@ -0,0 +1,8 @@
+/*	$OpenBSD: patchlevel.h,v 1.6 1998/01/17 20:30:26 millert Exp $	*/
+/* Id: patchlevel.h,v 1.30 1997/11/27 06:09:44 paulus Exp $ */
+
+#define	PATCHLEVEL	3
+
+#define VERSION		"2.3"
+#define IMPLEMENTATION	""
+#define DATE		"11 December 1997"
