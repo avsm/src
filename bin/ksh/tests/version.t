9ea06054c6c8f5c45b9bc9e1e58e502cf0e25cf1
branch-fixup <branch-fixup>
1998-06-25 19:02:47 +0000

    Add files from parent branch HEAD: 	bin/ksh/BUG-REPORTS 	bin/ksh/CONTRIBUTORS 	bin/ksh/ChangeLog 	bin/ksh/ChangeLog.0 	bin/ksh/IAFA-PACKAGE 	bin/ksh/NEWS 	bin/ksh/NOTES 	bin/ksh/PROJECTS 	bin/ksh/README 	bin/ksh/c_ksh.c 	bin/ksh/c_sh.c 	bin/ksh/c_test.c 	bin/ksh/config.h 	bin/ksh/edit.c 	bin/ksh/emacs.c 	bin/ksh/eval.c 	bin/ksh/expand.h 	bin/ksh/history.c 	bin/ksh/io.c 	bin/ksh/jobs.c 	bin/ksh/lex.c 	bin/ksh/lex.h 	bin/ksh/mail.c 	bin/ksh/main.c 	bin/ksh/misc.c 	bin/ksh/path.c 	bin/ksh/proto.h 	bin/ksh/sh.h 	bin/ksh/table.h 	bin/ksh/tests/alias.t 	bin/ksh/tests/eglob.t 	bin/ksh/tests/glob.t 	bin/ksh/tests/history.t 	bin/ksh/tests/regress.t 	bin/ksh/tests/syntax.t 	bin/ksh/tests/th 	bin/ksh/tests/th-sh 	bin/ksh/tests/unclass2.t 	bin/ksh/tests/version.t 	bin/ksh/tree.c 	bin/ksh/tree.h 	bin/ksh/var.c 	bin/ksh/version.c

---
+++ bin/ksh/tests/version.t
@@ -0,0 +1,8 @@
+name: version-1
+description:
+	Check version of shell.
+stdin:
+	echo $KSH_VERSION
+expected-stdout:
+	@(#)PD KSH v5.2.13 97/10/27
+---
---
+++ bin/ksh/version.c
@@ -0,0 +1,10 @@
+/*	$OpenBSD: version.c,v 1.6 1998/06/25 19:02:30 millert Exp $	*/
+
+/*
+ * value of $KSH_VERSION (or $SH_VERSION)
+ */
+
+#include "sh.h"
+
+const char ksh_version [] =
+	"@(#)PD KSH v5.2.13 97/10/27";
