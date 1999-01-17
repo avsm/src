c12aefa1ce176989c66db786c7a7b25c8dba5b1f
branch-fixup <branch-fixup>
1999-01-17 23:49:49 +0000

    Add files from parent branch HEAD: 	lib/libc_r/arch/hppa/uthread_machdep.h 	lib/libc_r/arch/m68k/uthread_machdep.h 	lib/libc_r/arch/mips/uthread_machdep.h 	lib/libc_r/arch/powerpc/uthread_machdep.h 	lib/libc_r/arch/sparc/uthread_machdep.h

---
+++ lib/libc_r/arch/powerpc/uthread_machdep.h
@@ -0,0 +1,33 @@
+/*
+ * OpenBSD/powerpc machine-dependent thread macros
+ *
+ * $OpenBSD: uthread_machdep.h,v 1.2 1999/01/17 23:49:49 d Exp $
+ */
+
+/* save the floating point state of a thread */
+#define _thread_machdep_save_float_state(thr) 		\
+	{						\
+		/* rahnds to fill in */			\
+	}
+
+/* restore the floating point state of a thread */
+#define _thread_machdep_restore_float_state(thr) 	\
+	{						\
+		/* rahnds to fill in */			\
+	}
+
+/* initialise the jmpbuf stack frame so it continues from entry */
+#define _thread_machdep_thread_create(thr, entry, pattr)	\
+	{						\
+		/* rahnds to fill in */			\
+	}
+
+#define	_thread_machdep_longjmp(a,v)	longjmp(a,v)
+#define	_thread_machdep_setjmp(a)	setjmp(a)
+
+typedef jmp_buf _machdep_jmp_buf;
+
+struct _machdep_struct {
+        char            xxx;
+};
+
