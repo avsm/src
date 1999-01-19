7b9333db493c69e435bf55b68de814cb1cc8fe55
branch-fixup <branch-fixup>
1999-01-19 20:41:59 +0000

    Add files from parent branch HEAD: 	bin/ksh/BUG-REPORTS 	bin/ksh/ChangeLog 	bin/ksh/NEWS 	bin/ksh/emacs.c 	bin/ksh/exec.c 	bin/ksh/expr.c 	bin/ksh/jobs.c 	bin/ksh/lex.c 	bin/ksh/sh.h 	bin/ksh/shf.c 	bin/ksh/tests/cdhist.t 	bin/ksh/tests/glob.t 	bin/ksh/tests/history.t 	bin/ksh/tests/regress.t 	bin/ksh/tests/th 	bin/ksh/tests/unclass1.t 	bin/ksh/tests/version.t 	bin/ksh/trap.c 	bin/ksh/tree.h 	bin/ksh/version.c

---
+++ bin/ksh/version.c
@@ -0,0 +1,10 @@
+/*	$OpenBSD: version.c,v 1.10 1999/01/19 20:41:56 millert Exp $	*/
+
+/*
+ * value of $KSH_VERSION (or $SH_VERSION)
+ */
+
+#include "sh.h"
+
+const char ksh_version [] =
+	"@(#)PD KSH v5.2.13.7 99/01/15";
