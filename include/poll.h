97e8dcbf5b58b1a5ac3bebd662dbbc2ffe3fcdb8
branch-fixup <branch-fixup>
2000-03-15 03:56:49 +0000

    Add files from parent branch HEAD: 	include/poll.h 	include/rpcsvc/yp_prot.h 	lib/libc/yp/yp_order.c 	sys/arch/mac68k/include/bus.h

---
+++ include/poll.h
@@ -0,0 +1,7 @@
+/*	$OpenBSD: poll.h,v 1.2 2000/03/15 03:56:48 todd Exp $ */
+
+/*
+ * Typical poll() implementations expect poll.h to be in /usr/include. 
+ * However this is not a convenient place for the real definitions.
+ */
+#include <sys/poll.h>
