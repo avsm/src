46a7bc12bba77932258acf762a385722f66a2318
branch-fixup <branch-fixup>
2000-04-05 05:35:28 +0000

    Add files from parent branch HEAD: 	sbin/ipf/ifaddr.c 	sbin/ipf/ifaddr.h 	sys/netinet/ip_nat.h 	sys/netinet/ip_proxy.h 	sys/netinet/ip_raudio_pxy.c

---
+++ sbin/ipf/ifaddr.h
@@ -0,0 +1,8 @@
+/* $OpenBSD: ifaddr.h,v 1.5 2000/04/05 05:35:28 kjell Exp $ */
+
+#ifndef        __IFADDR_H__
+#define        __IFADDR_H__
+
+int     if_addr __P((char *, struct in_addr *));
+
+#endif
