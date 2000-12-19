0df0062464b6facc1c25ff20ddaacf80f28ca655
Jason Wright <jason@openbsd.org>
2000-12-19 22:58:40 +0000

    Pull in patch from current: Errata (013): Procfs contained numerous overflows, which could lead an intruder to root permissions.  Procfs is NOT enabled by default in OpenBSD. Fix (provos): - strlcat and strlcpy into libkern. - calculate length and allocate buffer for status. work by deraadt@ and me

--- sys/lib/libkern/strlcpy.c
+++ sys/lib/libkern/strlcpy.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: strlcpy.c,v 1.1 2000/12/18 18:40:45 provos Exp $	*/
+/*	$OpenBSD: strlcpy.c,v 1.1.2.1 2000/12/19 22:58:39 jason Exp $	*/
 
 /*
  * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
@@ -28,7 +28,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char *rcsid = "$OpenBSD: strlcpy.c,v 1.1 2000/12/18 18:40:45 provos Exp $";
+static char *rcsid = "$OpenBSD: strlcpy.c,v 1.1.2.1 2000/12/19 22:58:39 jason Exp $";
 #endif /* LIBC_SCCS and not lint */
 
 #if !defined(_KERNEL) && !defined(_STANDALONE)
