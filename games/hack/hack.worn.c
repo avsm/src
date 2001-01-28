c6127323dac9f17e2cd922e2387f8c70d353f8bd
branch-fixup <branch-fixup>
2001-01-28 23:43:15 +0000

    Add files from parent branch HEAD: 	games/backgammon/teachgammon/Makefile 	games/cribbage/cribcur.h 	games/cribbage/pathnames.h 	games/fortune/Notes 	games/fortune/fortune/Makefile 	games/fortune/fortune/pathnames.h 	games/fortune/strfile/strfile.h 	games/fortune/tools/Do_spell 	games/fortune/tools/Do_troff 	games/fortune/tools/Troff.mac 	games/fortune/tools/do_sort 	games/fortune/unstr/Makefile 	games/hack/COPYRIGHT 	games/hack/Makequest 	games/hack/Original_READ_ME 	games/hack/READ_ME 	games/hack/date.h 	games/hack/def.edog.h 	games/hack/def.eshk.h 	games/hack/def.flag.h 	games/hack/def.func_tab.h 	games/hack/def.gen.h 	games/hack/def.gold.h 	games/hack/def.mkroom.h 	games/hack/def.obj.h 	games/hack/def.objclass.h 	games/hack/def.objects.h 	games/hack/def.permonst.h 	games/hack/def.trap.h 	games/hack/hack.apply.c 	games/hack/hack.do_name.c 	games/hack/hack.engrave.c 	games/hack/hack.main.c 	games/hack/hack.mfndpos.h 	games/hack/hack.mkmaze.c 	games/hack/hack.o_init.c 	games/hack/hack.objnam.c 	games/hack/hack.options.c 	games/hack/hack.potion.c 	games/hack/hack.rip.c 	games/hack/hack.rumors.c 	games/hack/hack.search.c 	games/hack/hack.sh 	games/hack/hack.shknam.c 	games/hack/hack.steal.c 	games/hack/hack.timeout.c 	games/hack/hack.topl.c 	games/hack/hack.track.c 	games/hack/hack.wield.c 	games/hack/hack.wizard.c 	games/hack/hack.worn.c 	games/hack/makedefs.c 	games/hack/pathnames.h 	games/hack/rnd.c 	games/hangman/pathnames.h 	games/larn/COPYRIGHT 	games/larn/Fixed.Bugs 	games/phantasia/COPYRIGHT 	games/phantasia/README 	games/phantasia/convert.c 	games/phantasia/map.c 	games/phantasia/oldplayer.h 	games/rogue/CHANGES 	games/rogue/USD.doc/Makefile 	games/rogue/pathnames.h 	games/sail/machdep.h 	games/trek/USD.doc/Makefile 	games/trek/board.x

---
+++ games/hack/hack.worn.c
@@ -0,0 +1,72 @@
+/*	$OpenBSD: hack.worn.c,v 1.2 2001/01/28 23:41:46 niklas Exp $	*/
+
+/*
+ * Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985.
+ */
+
+#ifndef lint
+static char rcsid[] = "$OpenBSD: hack.worn.c,v 1.2 2001/01/28 23:41:46 niklas Exp $";
+#endif /* not lint */
+
+#include "hack.h"
+
+struct worn {
+	long w_mask;
+	struct obj **w_obj;
+} worn[] = {
+	{ W_ARM, &uarm },
+	{ W_ARM2, &uarm2 },
+	{ W_ARMH, &uarmh },
+	{ W_ARMS, &uarms },
+	{ W_ARMG, &uarmg },
+	{ W_RINGL, &uleft },
+	{ W_RINGR, &uright },
+	{ W_WEP, &uwep },
+	{ W_BALL, &uball },
+	{ W_CHAIN, &uchain },
+	{ 0, 0 }
+};
+
+setworn(obj, mask)
+register struct obj *obj;
+long mask;
+{
+	register struct worn *wp;
+	register struct obj *oobj;
+
+	for(wp = worn; wp->w_mask; wp++) if(wp->w_mask & mask) {
+		oobj = *(wp->w_obj);
+		if(oobj && !(oobj->owornmask & wp->w_mask))
+			impossible("Setworn: mask = %ld.", wp->w_mask);
+		if(oobj) oobj->owornmask &= ~wp->w_mask;
+		if(obj && oobj && wp->w_mask == W_ARM){
+			if(uarm2) {
+				impossible("Setworn: uarm2 set?");
+			} else
+				setworn(uarm, W_ARM2);
+		}
+		*(wp->w_obj) = obj;
+		if(obj) obj->owornmask |= wp->w_mask;
+	}
+	if(uarm2 && !uarm) {
+		uarm = uarm2;
+		uarm2 = 0;
+		uarm->owornmask ^= (W_ARM | W_ARM2);
+	}
+}
+
+/* called e.g. when obj is destroyed */
+setnotworn(obj) register struct obj *obj; {
+	register struct worn *wp;
+
+	for(wp = worn; wp->w_mask; wp++)
+		if(obj == *(wp->w_obj)) {
+			*(wp->w_obj) = 0;
+			obj->owornmask &= ~wp->w_mask;
+		}
+	if(uarm2 && !uarm) {
+		uarm = uarm2;
+		uarm2 = 0;
+		uarm->owornmask ^= (W_ARM | W_ARM2);
+	}
+}
