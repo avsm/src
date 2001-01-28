4ba14ae9285b981f732cd60f350881c5fecd3610
branch-fixup <branch-fixup>
2001-01-28 22:38:49 +0000

    Add files from parent branch HEAD: 	sbin/isakmpd/GNUmakefile 	sbin/isakmpd/ike_phase_1.h 	sbin/isakmpd/sysdep/common/libsysdep/GNUmakefile 	sbin/isakmpd/sysdep/common/libsysdep/Makefile 	sbin/isakmpd/sysdep/common/libsysdep/cast.c 	sbin/isakmpd/sysdep/common/libsysdep/md5.c 	sbin/isakmpd/sysdep/common/libsysdep/sha1.c 	sbin/isakmpd/sysdep/common/md5.h 	sbin/isakmpd/sysdep/common/sha1.h 	sbin/isakmpd/sysdep/freebsd/sysdep-os.h 	sbin/isakmpd/sysdep/linux/Makefile.sysdep 	sbin/isakmpd/sysdep/linux/README 	sbin/isakmpd/sysdep/linux/sysdep-os.h 	sbin/isakmpd/sysdep/netbsd/sysdep-os.h 	sbin/isakmpd/sysdep/openbsd-encap/sysdep-os.h 	sbin/isakmpd/sysdep/openbsd/GNUmakefile.sysdep

---
+++ sbin/isakmpd/sysdep/common/libsysdep/cast.c
@@ -0,0 +1,31 @@
+/*	$OpenBSD: cast.c,v 1.2 2001/01/28 22:38:48 niklas Exp $	*/
+
+/* 
+ * Created by Martin Rinman, rinman@erlang.ericsson.se 
+ * Copyright (C) 1998 Ericsson Radio Systems AB 
+ */
+
+
+#include <assert.h>
+#include <sys/types.h>
+
+#include "cast.h"
+
+
+void
+cast_setkey(cast_key* key, u_int8_t* rawkey, int keybytes) {
+	assert("cast_setkey not implemented yet");
+}
+
+void
+cast_encrypt(cast_key* key, u_int8_t* inblock, u_int8_t* outblock) {
+	assert("cast_encrypt not implemented yet");
+}
+
+void
+cast_decrypt(cast_key* key, u_int8_t* inblock, u_int8_t* outblock) {
+	assert("cast_decrypt not implemented yet");
+}
+
+
+
