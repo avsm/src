64c2ae0a10acda22738cb762d818fac61533b561
branch-fixup <branch-fixup>
2001-01-28 23:43:15 +0000

    Add files from parent branch HEAD: 	games/backgammon/teachgammon/Makefile 	games/cribbage/cribcur.h 	games/cribbage/pathnames.h 	games/fortune/Notes 	games/fortune/README 	games/fortune/fortune/Makefile 	games/fortune/fortune/pathnames.h 	games/fortune/strfile/strfile.h 	games/fortune/tools/Do_spell 	games/fortune/tools/Do_troff 	games/fortune/tools/Troff.mac 	games/fortune/tools/do_sort 	games/fortune/unstr/Makefile 	games/hack/COPYRIGHT 	games/hack/Makequest 	games/hack/Original_READ_ME 	games/hack/READ_ME 	games/hack/alloc.c 	games/hack/config.h 	games/hack/date.h 	games/hack/def.edog.h 	games/hack/def.eshk.h 	games/hack/def.flag.h 	games/hack/def.func_tab.h 	games/hack/def.gen.h 	games/hack/def.gold.h 	games/hack/def.mkroom.h 	games/hack/def.monst.h 	games/hack/def.obj.h 	games/hack/def.objclass.h 	games/hack/def.objects.h 	games/hack/def.permonst.h 	games/hack/def.rm.h 	games/hack/def.trap.h 	games/hack/def.wseg.h 	games/hack/hack.6 	games/hack/hack.Decl.c 	games/hack/hack.apply.c 	games/hack/hack.bones.c 	games/hack/hack.c 	games/hack/hack.cmd.c 	games/hack/hack.do.c 	games/hack/hack.do_name.c 	games/hack/hack.do_wear.c 	games/hack/hack.dog.c 	games/hack/hack.eat.c 	games/hack/hack.end.c 	games/hack/hack.engrave.c 	games/hack/hack.fight.c 	games/hack/hack.fix 	games/hack/hack.h 	games/hack/hack.invent.c 	games/hack/hack.ioctl.c 	games/hack/hack.lev.c 	games/hack/hack.main.c 	games/hack/hack.makemon.c 	games/hack/hack.mfndpos.h 	games/hack/hack.mhitu.c 	games/hack/hack.mklev.c 	games/hack/hack.mkmaze.c 	games/hack/hack.mkobj.c 	games/hack/hack.mkshop.c 	games/hack/hack.mon.c 	games/hack/hack.monst.c 	games/hack/hack.o_init.c 	games/hack/hack.objnam.c 	games/hack/hack.options.c 	games/hack/hack.pager.c 	games/hack/hack.potion.c 	games/hack/hack.pri.c 	games/hack/hack.read.c 	games/hack/hack.rip.c 	games/hack/hack.rumors.c 	games/hack/hack.save.c 	games/hack/hack.search.c 	games/hack/hack.sh 	games/hack/hack.shk.c 	games/hack/hack.shknam.c 	games/hack/hack.steal.c 	games/hack/hack.termcap.c 	games/hack/hack.timeout.c 	games/hack/hack.topl.c 	games/hack/hack.track.c 	games/hack/hack.trap.c 	games/hack/hack.tty.c 	games/hack/hack.u_init.c 	games/hack/hack.unix.c 	games/hack/hack.vault.c 	games/hack/hack.version.c 	games/hack/hack.wield.c 	games/hack/hack.wizard.c 	games/hack/hack.worm.c 	games/hack/hack.worn.c 	games/hack/hack.zap.c 	games/hack/makedefs.c 	games/hack/pathnames.h 	games/hack/rnd.c 	games/hangman/pathnames.h 	games/larn/COPYRIGHT 	games/larn/Fixed.Bugs 	games/phantasia/COPYRIGHT 	games/phantasia/README 	games/phantasia/convert.c 	games/phantasia/map.c 	games/phantasia/oldplayer.h 	games/rogue/CHANGES 	games/rogue/USD.doc/Makefile 	games/rogue/USD.doc/rogue.me 	games/rogue/pathnames.h 	games/sail/machdep.h 	games/sail/sail.6 	games/trek/USD.doc/Makefile 	games/trek/board.x 	include/bsd_auth.h 	include/dbm.h 	include/skipjack.h

---
+++ games/hack/def.edog.h
@@ -0,0 +1,17 @@
+/*	$OpenBSD: def.edog.h,v 1.2 2001/01/28 23:41:42 niklas Exp $*/
+
+/*
+ * Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985.
+ *
+ *	$NetBSD: def.edog.h,v 1.3 1995/03/23 08:29:18 cgd Exp $
+ */
+
+struct edog {
+	long hungrytime;	/* at this time dog gets hungry */
+	long eattime;		/* dog is eating */
+	long droptime;		/* moment dog dropped object */
+	unsigned dropdist;		/* dist of drpped obj from @ */
+	unsigned apport;		/* amount of training */
+	long whistletime;		/* last time he whistled */
+};
+#define	EDOG(mp)	((struct edog *)(&(mp->mextra[0])))
---
+++ games/hack/def.gold.h
@@ -0,0 +1,17 @@
+/*	$OpenBSD: def.gold.h,v 1.2 2001/01/28 23:41:42 niklas Exp $*/
+
+/*
+ * Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985.
+ *
+ *	$NetBSD: def.gold.h,v 1.3 1995/03/23 08:29:27 cgd Exp $
+ */
+
+struct gold {
+	struct gold *ngold;
+	xchar gx,gy;
+	long amount;
+};
+
+extern struct gold *fgold;
+struct gold *g_at();
+#define newgold()	(struct gold *) alloc(sizeof(struct gold))
