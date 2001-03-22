af91dc7e11d0f1a7d5fcc2b2860e045f8ddf67a3
branch-fixup <branch-fixup>
2001-03-22 05:18:31 +0000

    Add files from parent branch HEAD: 	usr.bin/gprof/m68k.c 	usr.bin/gprof/m88k.c 	usr.bin/gprof/ns32k.c

---
+++ usr.bin/gprof/ns32k.c
@@ -0,0 +1,19 @@
+/*	$OpenBSD: ns32k.c,v 1.3 2001/03/22 05:18:30 mickey Exp $	*/
+/*	$NetBSD: ns32k.c,v 1.3 1995/04/19 07:16:13 cgd Exp $	*/
+
+#ifndef lint
+static char rcsid[] = "$OpenBSD: ns32k.c,v 1.3 2001/03/22 05:18:30 mickey Exp $";
+#endif /* not lint */
+
+#include "gprof.h"
+
+/*
+ * gprof -c isn't currently supported...
+ */
+void
+findcall( parentp , p_lowpc , p_highpc )
+    nltype		*parentp;
+    unsigned long	p_lowpc;
+    unsigned long	p_highpc;
+{
+}
