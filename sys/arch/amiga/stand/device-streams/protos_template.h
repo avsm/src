c462e4ce1444afa07f60c0a5edac33a9bc44bd8e
branch-fixup <branch-fixup>
2001-07-04 08:47:28 +0000

    Add files from parent branch HEAD: 	sys/arch/alpha/include/reloc.h 	sys/arch/amiga/stand/device-streams/devices.c 	sys/arch/amiga/stand/device-streams/devices.h 	sys/arch/amiga/stand/device-streams/devtostream.c 	sys/arch/amiga/stand/device-streams/getdevices.c 	sys/arch/amiga/stand/device-streams/getdevices.h 	sys/arch/amiga/stand/device-streams/protos.h 	sys/arch/amiga/stand/device-streams/protos_template.h 	sys/arch/amiga/stand/device-streams/rdbinfo.c 	sys/arch/amiga/stand/device-streams/smkfile 	sys/arch/amiga/stand/device-streams/streamtodev.c 	sys/arch/amiga/stand/device-streams/util.h 	sys/arch/m68k/include/sysctl.h 	sys/arch/mvmeppc/dev/openpicreg.h 	sys/arch/mvmeppc/stand/libbug/delay.c 	sys/arch/mvmeppc/stand/libbug/diskrd.c 	sys/arch/mvmeppc/stand/libbug/diskwr.c 	sys/arch/mvmeppc/stand/libbug/getbrdid.c 	sys/arch/mvmeppc/stand/libbug/instat.c 	sys/arch/mvmeppc/stand/libbug/netctrl.c 	sys/arch/mvmeppc/stand/libbug/netfopen.c 	sys/arch/mvmeppc/stand/libbug/netfread.c 	sys/arch/mvmeppc/stand/libbug/netrd.c 	sys/arch/mvmeppc/stand/libbug/netwr.c 	sys/arch/mvmeppc/stand/libbug/outch.c 	sys/arch/mvmeppc/stand/libbug/outln.c 	sys/arch/mvmeppc/stand/libbug/outstr.c 	sys/arch/mvmeppc/stand/libbug/return.c 	sys/arch/mvmeppc/stand/libbug/rtc_rd.c 	sys/arch/mvmeppc/stand/libsa/clock.c 	sys/arch/mvmeppc/stand/libsa/exec_mvme.c 	sys/arch/sparc/dev/bppreg.h 	sys/arch/sparc/include/bppioctl.h 	sys/arch/sun3/Makefile 	sys/arch/sun3/dev/README 	sys/arch/sun3/dev/btreg.h 	sys/arch/sun3/dev/bw2reg.h 	sys/arch/sun3/dev/cg4reg.h 	sys/arch/sun3/dev/if_iereg.h 	sys/arch/sun3/dev/memerr.h 	sys/arch/sun3/stand/Makefile 	sys/arch/sun3/stand/Makefile.inc 	sys/arch/sun3/stand/README 	sys/arch/sun3/stand/bootxx/Makefile 	sys/arch/sun3/stand/bootxx/conf.c 	sys/arch/sun3/stand/installboot/Makefile 	sys/arch/sun3/stand/libsa/SRT0.S 	sys/arch/sun3/stand/libsa/SRT1.c 	sys/arch/sun3/stand/libsa/clock.c 	sys/arch/sun3/stand/libsa/clock.h 	sys/arch/sun3/stand/libsa/dev_disk.c 	sys/arch/sun3/stand/libsa/dvma.h 	sys/arch/sun3/stand/libsa/gets.c 	sys/arch/sun3/stand/libsa/panic.c 	sys/arch/sun3/stand/libsa/promboot.c 	sys/arch/sun3/stand/libsa/promboot.h 	sys/arch/sun3/stand/libsa/promcons.c 	sys/arch/sun3/stand/libsa/promdev.h 	sys/arch/sun3/stand/netboot/Makefile 	sys/arch/sun3/stand/netboot/boot.c 	sys/arch/sun3/stand/netboot/conf.c 	sys/arch/sun3/stand/tapeboot/Makefile 	sys/arch/sun3/stand/tapeboot/boot.c 	sys/arch/sun3/stand/tapeboot/conf.c 	sys/arch/sun3/stand/ufsboot/Makefile 	sys/arch/sun3/stand/ufsboot/boot.c 	sys/arch/sun3/stand/ufsboot/conf.c

---
+++ sys/arch/amiga/stand/device-streams/protos_template.h
@@ -0,0 +1,10 @@
+/*	$OpenBSD: protos_template.h,v 1.2 2001/07/04 08:44:58 niklas Exp $	*/
+
+/* START: "system" */
+#include <clib/alib_protos.h>
+#include <clib/exec_protos.h>
+#include <clib/dos_protos.h>
+#include <pragmas/exec_pragmas.h>
+#include <pragmas/dos_pragmas.h>
+extern struct Library *DOSBase;
+/* END: "system" */
