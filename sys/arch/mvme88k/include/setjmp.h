b2c7bdaba0f740cdb851ccb24ebc33f3a0faf37e
branch-fixup <branch-fixup>
2001-08-12 12:03:03 +0000

    Add files from parent branch HEAD: 	lib/libc/arch/m68k/sys/sigreturn.S 	lib/libc/arch/vax/sys/sigreturn.S 	lib/libc/stdlib/abort.c 	lib/libsectok/ifdhandler.c 	sys/arch/alpha/alpha/db_instruction.h 	sys/arch/amiga/stand/boot/amigagraph.h 	sys/arch/mvme68k/mvme68k/vectors.s 	sys/arch/mvme88k/include/endian.h 	sys/arch/mvme88k/include/exec.h 	sys/arch/mvme88k/include/ieee.h 	sys/arch/mvme88k/include/proc.h 	sys/arch/mvme88k/include/profile.h 	sys/arch/mvme88k/include/ptrace.h 	sys/arch/mvme88k/include/setjmp.h 	sys/arch/mvme88k/include/vid.h 	sys/arch/vax/include/ka650.h 	sys/arch/vax/include/pte.h 	sys/lib/libkern/arch/ns32k/SYS.h 	usr.sbin/amd/amd/info_hes.c 	usr.sbin/mopd/common/common.h 	usr.sbin/mopd/common/file.c 	usr.sbin/mopd/common/mopdef.h 	usr.sbin/mopd/common/nmadef.h 	usr.sbin/mopd/common/os.h

---
+++ sys/arch/mvme88k/include/setjmp.h
@@ -0,0 +1,9 @@
+/*	$OpenBSD: setjmp.h,v 1.6 2001/08/12 12:03:02 heko Exp $	*/
+
+/*
+ * machine/setjmp.h: machine dependent setjmp-related information.
+ */
+#ifndef __MACHINE_SETJMP_H__
+#define __MACHINE_SETJMP_H__
+#define	_JBLEN	22		/* size, in longs, of a jmp_buf */
+#endif /* __MACHINE_SETJMP_H__ */
