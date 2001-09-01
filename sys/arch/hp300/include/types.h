8048b477833e12c245060af9b73c72a08c0fc364
branch-fixup <branch-fixup>
2001-09-01 05:48:18 +0000

    Add files from parent branch HEAD: 	sys/arch/alpha/include/autoconf.h 	sys/arch/hp300/include/types.h

---
+++ sys/arch/hp300/include/types.h
@@ -0,0 +1,11 @@
+/*	$OpenBSD: types.h,v 1.3 2001/09/01 05:48:18 jason Exp $	*/
+/*	$NetBSD: types.h,v 1.8 1996/12/17 08:11:50 thorpej Exp $	*/
+
+#ifndef _MACHINE_TYPES_H_
+#define _MACHINE_TYPES_H_
+
+#include <m68k/types.h>
+
+#define __HAVE_DEVICE_REGISTER
+
+#endif
