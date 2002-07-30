3c4e99e72c9cb80e1bd5635bd14e1414a084e358
Miod Vallat <miod@openbsd.org>
2002-07-30 07:29:13 +0000

    Errata #028 (millert): Replace an instance of chmod() with fchmod()

--- usr.sbin/pppd/main.c
+++ usr.sbin/pppd/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.31 2001/05/15 19:56:06 deraadt Exp $	*/
+/*	$OpenBSD: main.c,v 1.31.2.1 2002/07/30 07:29:13 miod Exp $	*/
 
 /*
  * main.c - Point-to-Point Protocol main module
@@ -23,7 +23,7 @@
 #if 0
 static char rcsid[] = "Id: main.c,v 1.49 1998/05/05 05:24:17 paulus Exp $";
 #else
-static char rcsid[] = "$OpenBSD: main.c,v 1.31 2001/05/15 19:56:06 deraadt Exp $";
+static char rcsid[] = "$OpenBSD: main.c,v 1.31.2.1 2002/07/30 07:29:13 miod Exp $";
 #endif
 #endif
 
@@ -797,7 +797,7 @@ close_tty()
     restore_tty(ttyfd);
 
     if (tty_mode != (mode_t) -1)
-	chmod(devnam, tty_mode);
+	fchmod(ttyfd, tty_mode);
 
     close(ttyfd);
     ttyfd = -1;
