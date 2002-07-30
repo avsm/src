e4996ce43c7b111469a68bb715bca812fdc7003e
Jason Wright <jason@openbsd.org>
2002-07-30 14:46:12 +0000

    Pull in patch from current: Fix (millert), errata 011: Replace an instance of chmod() with fchmod()

--- usr.sbin/pppd/main.c
+++ usr.sbin/pppd/main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: main.c,v 1.34 2002/02/19 19:39:40 millert Exp $	*/
+/*	$OpenBSD: main.c,v 1.34.2.1 2002/07/30 14:46:12 jason Exp $	*/
 
 /*
  * main.c - Point-to-Point Protocol main module
@@ -23,7 +23,7 @@
 #if 0
 static char rcsid[] = "Id: main.c,v 1.49 1998/05/05 05:24:17 paulus Exp $";
 #else
-static char rcsid[] = "$OpenBSD: main.c,v 1.34 2002/02/19 19:39:40 millert Exp $";
+static char rcsid[] = "$OpenBSD: main.c,v 1.34.2.1 2002/07/30 14:46:12 jason Exp $";
 #endif
 #endif
 
@@ -799,7 +799,7 @@ close_tty()
     restore_tty(ttyfd);
 
     if (tty_mode != (mode_t) -1)
-	chmod(devnam, tty_mode);
+	fchmod(ttyfd, tty_mode);
 
     close(ttyfd);
     ttyfd = -1;
