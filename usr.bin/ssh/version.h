9a5e932d8acd028a5321bd68451fb554765acebf
margarida <margarida>
2003-05-11 12:01:53 +0000

    Update OpenSSH to version 3.6.1

--- usr.bin/ssh/version.h
+++ usr.bin/ssh/version.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: version.h,v 1.36 2003/03/17 11:43:47 markus Exp $ */
+/* $OpenBSD: version.h,v 1.36.2.1 2003/05/11 12:01:53 margarida Exp $ */
 
-#define SSH_VERSION	"OpenSSH_3.6"
+#define SSH_VERSION	"OpenSSH_3.6.1"
