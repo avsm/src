770e8a9918cc9beafb62ebbd1dc86f15f7947c94
Brad Smith <brad@openbsd.org>
2003-07-16 18:04:56 +0000

    MFC: Fix by tedu@
    reset inode fields if deleted, prevents a panic after deleting a device.
    from netbsd via otto moerbeek
    
    ok deraadt@

--- sys/ufs/ext2fs/ext2fs_vfsops.c
+++ sys/ufs/ext2fs/ext2fs_vfsops.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ext2fs_vfsops.c,v 1.25 2002/07/29 17:45:20 fgsch Exp $	*/
+/*	$OpenBSD: ext2fs_vfsops.c,v 1.25.4.1 2003/07/16 18:04:56 brad Exp $	*/
 /*	$NetBSD: ext2fs_vfsops.c,v 1.1 1997/06/11 09:34:07 bouyer Exp $	*/
 
 /*
@@ -899,6 +899,11 @@ ext2fs_vget(mp, ino, vpp)
 				&ip->i_din, sizeof(struct ext2fs_dinode));
 	ip->i_effnlink = ip->i_e2fs_nlink;
 	brelse(bp);
+
+	/* If the inode was deleted, reset all fields */
+	if (ip->i_e2fs_dtime != 0) {
+		ip->i_e2fs_mode = ip->i_e2fs_size = ip->i_e2fs_nblock = 0;
+	}
 
 	/*
 	 * Initialize the vnode from the inode, check for aliases.
