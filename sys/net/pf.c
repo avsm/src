18053624f7bd25da8aac4ad9d59de2826af00ec8
Brad Smith <brad@openbsd.org>
2003-08-09 18:23:10 +0000

    MFC: Fix by mcbride@
    With rdr we want the source IP from the packet, not the source IP from
    the rule. Fixes rdr with address pools using bitmask and source-hash
    address selection methods.
    
    ok dhartmei@ henning@
    
    ok deraadt@

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.328.2.3 2003/07/10 22:34:28 brad Exp $ */
+/*	$OpenBSD: pf.c,v 1.328.2.4 2003/08/09 18:23:10 brad Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -1756,8 +1756,7 @@ pf_get_translation(int direction, struct ifnet *ifp, u_int8_t proto,
 			}
 			break;
 		case PF_RDR: {
-			if (pf_map_addr(r->af, &r->rpool,
-			    &r->src.addr.v.a.addr, naddr, NULL))
+			if (pf_map_addr(r->af, &r->rpool, saddr, naddr, NULL))
 				return (NULL);
 
 			if (r->rpool.proxy_port[1]) {
