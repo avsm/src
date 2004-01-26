ab834bc08715495645610e14c346333e92bb743b
branch-fixup <branch-fixup>
2004-01-26 20:04:11 +0000

    Add files from parent branch HEAD: 	lib/csu/common_elf/crtbegin.c 	lib/csu/common_elf/crtbeginS.c 	lib/csu/common_elf/crtend.c 	lib/csu/common_elf/crtendS.c

---
+++ lib/csu/common_elf/crtend.c
@@ -0,0 +1,14 @@
+/*	$OpenBSD: crtend.c,v 1.5 2004/01/26 20:04:11 espie Exp $	*/
+/*	$NetBSD: crtend.c,v 1.1 1996/09/12 16:59:04 cgd Exp $	*/
+
+#include <sys/cdefs.h>
+#include "md_init.h"
+#include "extern.h"
+
+static init_f __CTOR_LIST__[1]
+    __attribute__((section(".ctors"))) = { (void *)0 };		/* XXX */
+static init_f __DTOR_LIST__[1]
+    __attribute__((section(".dtors"))) = { (void *)0 };		/* XXX */
+
+MD_SECTION_EPILOGUE(".init");
+MD_SECTION_EPILOGUE(".fini");
