4ec199e3bc52218885ba0aa4e2b177cc193c16b3
Brad Smith <brad@openbsd.org>
2004-06-11 03:08:03 +0000

    MFC: Fix by hshoexer@
    Mark authenticated messages explicitly.  Better check for authentication before
    deleteing SAs.
    
    This fix is needed to solve the problems reported by Thomas Walpuski, previous
    diff was not sufficient.  Pointed out by Thomas.  Thanks!

--- sbin/isakmpd/ike_phase_1.c
+++ sbin/isakmpd/ike_phase_1.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ike_phase_1.c,v 1.39 2003/08/08 08:46:59 ho Exp $	*/
+/*	$OpenBSD: ike_phase_1.c,v 1.39.2.1 2004/06/11 03:08:02 brad Exp $	*/
 /*	$EOM: ike_phase_1.c,v 1.31 2000/12/11 23:47:56 niklas Exp $	*/
 
 /*
@@ -1093,6 +1093,9 @@ ike_phase_1_recv_AUTH (struct message *msg)
       /* XXX Log?  */
       return -1;
     }
+
+  /* Mark message as authenticated. */
+  msg->flags |= MSG_AUTHENTICATED;
 
   return 0;
 }
