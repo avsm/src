42598a598e9e07a6d0ca91c01fbeeaf7a55cfade
Brad Smith <brad@openbsd.org>
2004-11-02 01:05:50 +0000

    MFC: Fix by krw@
    Fix 'Tape block size (32754) not a multiple of dump block size
    (1024).' errors in restore(1).
    
    Check for SDEV_OPEN in sc_link->flags (where it lives) rather than
    sense->flags (where it doesn't).
    
    Problem noted by John Danks on misc@ and reinforced by Otto & Hugh.
    
    ok deraadt@ krw@

--- sys/scsi/st.c
+++ sys/scsi/st.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: st.c,v 1.41 2004/08/01 23:01:06 marco Exp $	*/
+/*	$OpenBSD: st.c,v 1.41.2.1 2004/11/02 01:05:50 brad Exp $	*/
 /*	$NetBSD: st.c,v 1.71 1997/02/21 23:03:49 thorpej Exp $	*/
 
 /*
@@ -1815,7 +1815,7 @@ st_interpret_sense(xs)
 	u_int8_t skey = sense->flags & SSD_KEY;
 	int32_t info;
 
-	if (((sense->flags & SDEV_OPEN) == 0) ||
+	if (((sc_link->flags & SDEV_OPEN) == 0) ||
 	    (serr != 0x70 && serr != 0x71))
 		return (EJUSTRETURN); /* let the generic code handle it */
 
