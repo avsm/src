2818c00ce156630dc5be46bfb28d1608634dcb3c
Brad Smith <brad@openbsd.org>
2005-01-11 04:36:24 +0000

    MFC: Fix by mcbride@
    Make sure bogus values don't make their way into tcp_xmit_timer() calculations.
    - Convert out-of-range rtt values to valid ones in tcp_xmit_timer().
    
    ok frantzen@ markus@ deraadt@ mcbride@

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_input.c,v 1.175 2004/07/16 09:26:07 markus Exp $	*/
+/*	$OpenBSD: tcp_input.c,v 1.175.2.1 2005/01/11 04:36:23 brad Exp $	*/
 /*	$NetBSD: tcp_input.c,v 1.23 1996/02/13 23:43:44 christos Exp $	*/
 
 /*
@@ -2792,8 +2792,13 @@ tcp_xmit_timer(tp, rtt)
 	short delta;
 	short rttmin;
 
-	tcpstat.tcps_rttupdated++;
 	--rtt;
+	if (rtt < 0)
+		rtt = 0;
+	if (rtt > TCP_RTT_MAX)
+		rtt = TCP_RTT_MAX;
+
+	tcpstat.tcps_rttupdated++;
 	if (tp->t_srtt != 0) {
 		/*
 		 * srtt is stored as fixed point with 3 bits after the
