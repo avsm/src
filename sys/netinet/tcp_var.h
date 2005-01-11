350008c73087d0a1b7a6bf88592e902a787147fb
Brad Smith <brad@openbsd.org>
2005-01-11 04:40:30 +0000

    MFC: Fix by mcbride@
    Make sure bogus values don't make their way into tcp_xmit_timer() calculations.
    - Convert out-of-range rtt values to valid ones in tcp_xmit_timer().
    
    ok frantzen@ markus@ deraadt@ mcbride@

--- sys/netinet/tcp_var.h
+++ sys/netinet/tcp_var.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_var.h,v 1.61 2004/03/02 12:51:12 markus Exp $	*/
+/*	$OpenBSD: tcp_var.h,v 1.61.2.1 2005/01/11 04:40:30 brad Exp $	*/
 /*	$NetBSD: tcp_var.h,v 1.17 1996/02/13 23:44:24 christos Exp $	*/
 
 /*
@@ -325,6 +325,7 @@ tcp_reass_unlock(struct tcpcb *tp)
 #define	TCP_RTT_SHIFT		3	/* shift for srtt; 3 bits frac. */
 #define	TCP_RTTVAR_SCALE	4	/* multiplier for rttvar; 2 bits */
 #define	TCP_RTTVAR_SHIFT	2	/* multiplier for rttvar; 2 bits */
+#define TCP_RTT_MAX		(1<<9)	/* maximum rtt */
 
 /*
  * The initial retransmission should happen at rtt + 4 * rttvar.
