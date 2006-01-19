bf963d8a3e100ba6927d4e94a9097de425f1ffc5
Brad Smith <brad@openbsd.org>
2006-01-19 21:51:36 +0000

    MFC: Fix by dhartmei@
    fix a bug in the fragment cache (used for 'scrub fragment crop/drop-ovl',
    but not 'fragment reassemble'), which can cause some fragments to get
    inserted into the cache twice, thereby violating an invariant, and panic-
    ing the system subsequently.
    
    ok deraadt@ dhartmei@

--- sys/net/pf_norm.c
+++ sys/net/pf_norm.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf_norm.c,v 1.97 2004/09/21 16:59:12 aaron Exp $ */
+/*	$OpenBSD: pf_norm.c,v 1.97.2.1 2006/01/19 21:51:36 brad Exp $ */
 
 /*
  * Copyright 2001 Niels Provos <provos@citi.umich.edu>
@@ -704,7 +704,7 @@ pf_fragcache(struct mbuf **m0, struct ip *h, struct pf_fragment **frag, int mff,
 			} else {
 				hosed++;
 			}
-		} else {
+		} else if (frp == NULL) {
 			/* There is a gap between fragments */
 			DPFPRINTF(("fragcache[%d]: gap %d %d-%d (%d-%d)\n",
 			    h->ip_id, -aftercut, off, max, fra->fr_off,
