de950680c50c888c7dde4a01615a7ebe741eed24
Brad Smith <brad@openbsd.org>
2006-05-06 18:51:07 +0000

    MFC: Fix by damien@
    allows dhcpd to work when ral/ural is acting as a standalone AP (without
    bridging).  it appears that ieee80211_input() enqueues packets in if_snd
    without calling if_start().
    
    ok damien@

--- sys/dev/usb/if_ral.c
+++ sys/dev/usb/if_ral.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ral.c,v 1.44 2005/08/27 14:46:37 damien Exp $  */
+/*	$OpenBSD: if_ral.c,v 1.44.2.1 2006/05/06 18:51:07 brad Exp $  */
 
 /*-
  * Copyright (c) 2005
@@ -925,6 +925,13 @@ ural_rxeof(usbd_xfer_handle xfer, usbd_private_handle priv, usbd_status status)
 
 	/* node is no longer needed */
 	ieee80211_release_node(ic, ni);
+
+	/*
+	 * In HostAP mode, ieee80211_input() will enqueue packets in if_snd
+	 * without calling if_start().
+	 */
+	if (!IFQ_IS_EMPTY(&ifp->if_snd) && !(ifp->if_flags & IFF_OACTIVE))
+		ural_start(ifp);
 
 	splx(s);
 
