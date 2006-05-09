3033d7f23b993068e0a063ae5071f1994187911d
Brad Smith <brad@openbsd.org>
2006-05-09 18:41:17 +0000

    MFC: Fix by pedro@
    Make ath_attach() return error if the RF is unsupported so the PCI code
    can disable its shutdown hooks, otherwise we will panic upon halt.
    
    ok deraadt@ pedro@

--- sys/dev/ic/ath.c
+++ sys/dev/ic/ath.c
@@ -1,4 +1,4 @@
-/*      $OpenBSD: ath.c,v 1.46 2006/02/20 20:12:13 damien Exp $  */
+/*      $OpenBSD: ath.c,v 1.46.2.1 2006/05/09 18:41:17 brad Exp $  */
 /*	$NetBSD: ath.c,v 1.37 2004/08/18 21:59:39 dyoung Exp $	*/
 
 /*-
@@ -255,6 +255,7 @@ ath_attach(u_int16_t devid, struct ath_softc *sc)
 	if (ah->ah_radio_5ghz_revision >= AR5K_SREV_RAD_UNSUPP ||
 	    ah->ah_radio_2ghz_revision >= AR5K_SREV_RAD_UNSUPP) {
 		printf(": RF radio not supported\n");
+		error = EOPNOTSUPP;
 		goto bad;
 	}
 
