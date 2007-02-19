da32e42d7a16f0b13e64e203462836eef6da23d1
branch-fixup <branch-fixup>
2007-02-19 17:18:43 +0000

    Add files from parent branch HEAD: 	sys/arch/alpha/include/atomic.h 	sys/arch/amd64/include/atomic.h 	sys/arch/arm/include/atomic.h 	sys/arch/armish/include/atomic.h 	sys/arch/aviion/include/atomic.h 	sys/arch/cats/include/atomic.h 	sys/arch/hp300/include/atomic.h 	sys/arch/hppa/include/atomic.h 	sys/arch/hppa64/include/atomic.h 	sys/arch/i386/include/atomic.h 	sys/arch/landisk/include/atomic.h 	sys/arch/luna88k/include/atomic.h 	sys/arch/m68k/include/atomic.h 	sys/arch/m88k/include/atomic.h 	sys/arch/mac68k/include/atomic.h 	sys/arch/macppc/include/atomic.h 	sys/arch/mips64/include/atomic.h 	sys/arch/mvme68k/include/atomic.h 	sys/arch/mvme88k/include/atomic.h 	sys/arch/mvmeppc/include/atomic.h 	sys/arch/powerpc/include/atomic.h 	sys/arch/sgi/include/atomic.h 	sys/arch/sh/include/atomic.h 	sys/arch/solbourne/include/atomic.h 	sys/arch/sparc/include/atomic.h 	sys/arch/sparc64/include/atomic.h 	sys/arch/vax/include/atomic.h 	sys/arch/zaurus/include/atomic.h

---
+++ sys/arch/vax/include/atomic.h
@@ -0,0 +1,23 @@
+/*	$OpenBSD: atomic.h,v 1.2 2007/02/19 17:18:43 deraadt Exp $	*/
+
+/* Public Domain */
+
+#ifndef __VAX_ATOMIC_H__
+#define __VAX_ATOMIC_H__
+
+#if defined(_KERNEL)
+
+static __inline void
+atomic_setbits_int(__volatile unsigned int *uip, unsigned int v)
+{
+	*uip |= v;
+}
+
+static __inline void
+atomic_clearbits_int(__volatile unsigned int *uip, unsigned int v)
+{
+	*uip &= ~v;
+}
+
+#endif /* defined(_KERNEL) */
+#endif /* __VAX_ATOMIC_H__ */
