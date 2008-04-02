7b4fda758c5c11d67994a641b4761201fe803cf1
branch-fixup <branch-fixup>
2008-04-02 21:53:18 +0000

    Add files from parent branch HEAD: 	sys/arch/mvme88k/stand/bootsd/Makefile 	sys/arch/mvme88k/stand/bootsd/version.c 	sys/arch/mvme88k/stand/bootst/Makefile 	sys/arch/mvme88k/stand/bootst/version.c 	sys/arch/mvme88k/stand/bootxx/Makefile 	sys/arch/mvme88k/stand/bootxx/bootxx.c 	sys/arch/mvme88k/stand/bootxx/version.c 	sys/arch/mvme88k/stand/bugcrt/Makefile 	sys/arch/mvme88k/stand/bugcrt/Makefile.inc 	sys/arch/mvme88k/stand/bugcrt/crt0.S 	sys/arch/mvme88k/stand/libbug/libbug.h 	sys/arch/mvme88k/stand/libsa/Makefile 	sys/arch/mvme88k/stand/netboot/Makefile 	sys/arch/mvme88k/stand/netboot/boot.c 	sys/arch/mvme88k/stand/netboot/version.c 	sys/arch/mvme88k/stand/tftpboot/Makefile 	sys/arch/mvme88k/stand/tftpboot/boot.c 	sys/arch/mvme88k/stand/tftpboot/version.c

---
+++ sys/arch/mvme88k/stand/tftpboot/version.c
@@ -0,0 +1,9 @@
+/*	$OpenBSD: version.c,v 1.4 2008/04/02 21:53:18 miod Exp $ */
+
+/*
+ *	1.4	rewritten crt code, self-relocatable
+ *	1.3	kernel loaded with loadfile, a.out and ELF formats
+ *	1.2	rewritten startup code and general cleanup
+ *	1.1	initial revision
+ */
+char *version = "1.4";
