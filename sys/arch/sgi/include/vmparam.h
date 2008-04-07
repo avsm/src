bcebf3fd8892a462502154f8622baca7c798cf5b
branch-fixup <branch-fixup>
2008-04-07 22:41:52 +0000

    Add files from parent branch HEAD: 	sys/arch/sgi/include/memconf.h 	sys/arch/sgi/include/vmparam.h

---
+++ sys/arch/sgi/include/vmparam.h
@@ -0,0 +1,10 @@
+/*	$OpenBSD: vmparam.h,v 1.2 2008/04/07 22:41:52 miod Exp $ */
+
+#ifndef _SGI_VMPARAM_H_
+#define _SGI_VMPARAM_H_
+
+#define	VM_PHYSSEG_MAX	32	/* Max number of physical memory segments */
+
+#include <mips64/vmparam.h>
+
+#endif	/* _SGI_VMPARAM_H_ */
