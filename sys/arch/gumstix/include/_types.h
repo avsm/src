54008ade413616b2f591121e9e788903de08c82c
branch-fixup <branch-fixup>
2008-11-27 20:51:48 +0000

    Add files from parent branch HEAD: 	sys/arch/gumstix/include/_types.h 	sys/arch/gumstix/include/atomic.h 	sys/arch/gumstix/include/conf.h 	sys/arch/gumstix/include/gumstix_var.h 	sys/arch/gumstix/include/param.h 	sys/arch/gumstix/include/spinlock.h

---
+++ sys/arch/gumstix/include/_types.h
@@ -0,0 +1,11 @@
+/* 	$OpenBSD: _types.h,v 1.2 2008/11/27 20:51:48 miod Exp $	*/
+
+#ifndef _GUMSTIX__TYPES_H_
+#define	_GUMSTIX__TYPES_H_
+
+#include <arm/_types.h>
+
+#define __HAVE_GENERIC_SOFT_INTERRUPTS
+#define __HAVE_TIMECOUNTER
+
+#endif
