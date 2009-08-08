4e1961758128363fc54e784d1dee0578a829e971
Henning Brauer <henning@openbsd.org>
2009-08-08 10:41:41 +0000

    reveal identidy

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.99 2009/07/01 15:10:25 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.99.4.1 2009/08/08 10:41:41 henning Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -65,7 +65,7 @@ ost="OpenBSD"
 osr="4.6"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"			/* release */
 #if 0
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
