1d6b8110bf59abd0f8a4ed62b2bafeb43a12dd9b
Claudio Jeker <claudio@openbsd.org>
2009-10-28 20:21:05 +0000

    MFC rev. 1.196 of ip_output.c by deraadt@:
    *NULL store in IP_AUTH_LEVEL, IP_ESP_TRANS_LEVEL, IP_ESP_NETWORK_LEVEL,
    IP_IPCOMP_LEVEL found by Clement LECIGNE, localhost root exploitable on
    userland/kernel shared vm machines (ie. i386, amd64, arm, sparc (but not
    sparc64), sh, ...) on OpenBSD 4.3 or older
    ok claudio

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_output.c,v 1.191 2008/05/09 02:56:36 markus Exp $	*/
+/*	$OpenBSD: ip_output.c,v 1.191.2.1 2009/10/28 20:21:05 claudio Exp $	*/
 /*	$NetBSD: ip_output.c,v 1.28 1996/02/13 23:43:07 christos Exp $	*/
 
 /*
@@ -1485,6 +1485,7 @@ ip_ctloutput(op, so, level, optname, mp)
 		case IP_ESP_TRANS_LEVEL:
 		case IP_ESP_NETWORK_LEVEL:
 		case IP_IPCOMP_LEVEL:
+			*mp = m = m_get(M_WAIT, MT_SOOPTS);
 #ifndef IPSEC
 			m->m_len = sizeof(int);
 			*mtod(m, int *) = IPSEC_LEVEL_NONE;
